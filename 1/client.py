440e6faf38d7c17745e9edd582bbcf6a0c9f5254
Kyle Shteynberg <ks1229@scarletmail.rutgers.edu>
2020-02-22 18:14:07 -0500

    creates RESOLVE.txt and writes results to it

--- 1/client.py
+++ 1/client.py
@@ -4,54 +4,78 @@ import time
 import random
 import socket
 
-HNS = open("PROJI-HNS.txt","r")
-HNS_TABLE = {}
-for line in HNS:
-    entry = line.split()
-    HNS_TABLE = entry[0]
+result = []
 
-    def client():
-        try:
-            cs = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            print("[C]: Client socket created")
-        except socket.error as err:
-            print('socket open error: {} \n'.format(err))
-            exit()
+def client():
+    try:
+        cs = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    except socket.error as err:
+        print('socket open error: {} \n'.format(err))
+        exit()
 
-        # Define the port on which you want to connect to the server
-        port = int(sys.argv[2])
-        localhost_addr = socket.gethostbyname(sys.argv[1])
+    # Define the port on which you want to connect to the server
+    port = int(sys.argv[2])
+    localhost_addr = socket.gethostbyname(sys.argv[1])
+    localhost_addr2 = ""
 
-        # connect to the server on local machine
-        server_binding = (localhost_addr, port)
-        cs.connect(server_binding)
+    # connect to the server on local machine
+    server_binding = (localhost_addr, port)
+    cs.connect(server_binding)
 
-        # send data to server
-        msg = HNS_TABLE[0]
-        cs.send(msg.encode('utf-8'))
+    TS_NEEDED = []
+    for line in HNS_TABLE:
+        cs.send(line.encode('utf-8'))
 
         # Receive data from the server
-        data_from_server = cs.recv(100)
-        print("[C]: Data received from server: {}".format(data_from_server.decode('utf-8')))
-
-        # close the client socket
-        cs.close()
+        data_from_server = cs.recv(200)
 
         # if rs comes back empty, connect to ts
         if data_from_server.__contains__(" - NS"):
+            localhost_addr2 = data_from_server[0:len(data_from_server) - 5]
+            TS_NEEDED.append(line)
+        else:
+            result.append(data_from_server)
 
-            port = int(sys.argv[3])
-            localhost_addr = data_from_server[0:len(data_from_server)-5]
-            # connect to the server on local machine
-            server_binding = (localhost_addr, port)
-            cs.connect(server_binding)
 
-            # send data to server
-            msg = HNS_TABLE[0]
-            cs.send(msg.encode('utf-8'))
 
-            data_from_server = cs.recv(100)
-            print("[C]: Data received from server: {}".format(data_from_server.decode('utf-8')))
+    # send data to server
 
-        exit()
 
+    # close the client socket
+    cs.close()
+
+    if TS_NEEDED:
+        try:
+            cs2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        except socket.error as err:
+            print('socket open error: {} \n'.format(err))
+            exit()
+        port2 = int(sys.argv[3])
+
+        # connect to the server on local machine
+
+        cs2.connect((localhost_addr2, port2))
+
+        for line in TS_NEEDED:
+            cs2.send(line.encode('utf-8'))
+
+            data_from_server2 = cs2.recv(200)
+            result.append(data_from_server2)
+
+        cs2.close()
+
+    f = open("RESOLVED.txt", "w+")
+    for line in result:
+        f.write("%s\r\n" % (line))
+        print(line)
+    exit()
+
+HNS = open("PROJI-HNS.txt","r")
+HNS_TABLE = []
+for line in HNS:
+    entry = line.split()
+    HNS_TABLE.append(entry[0])
+
+
+
+client()
